b9e09ab9bf12f3d7baa8ebfd55c332b551be0876
Mukhammadislom <diyorend@gmail.com>
2023-02-23 17:43:29 +0500

    Add project files.

---
+++ BankConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using BankConsoleApp;
+
+var account = new BankAccount("Kendra", 100001);
+Console.WriteLine(
+    $"Account {account.Number} created for " +
+    $"{account.Owner} and balance is {account.Balance}");
+
+account.MakeWithdrawal(220, DateTime.Now, "Ticket");
+account.MakeWithdrawal(50, DateTime.Now, "Hammock");
+account.MakeWithdrawal(100, DateTime.Now, "Registration");
+account.MakeWithdrawal(30, DateTime.Now, "Lunch");
+
+Console.WriteLine(account.GetAccountHistory());
